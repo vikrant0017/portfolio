dd0f321844a70ffdefe7077b2185cf9b7f6f9657
Vikrant Pradhan <96116296+vikrant0017@users.noreply.github.com>
2024-03-29 06:58:29 +0000

    update user info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Vikrant Pradhan",
+  title: "Software Developer",
+  email: "pradhan.vikrant.17@gmail.com",
+  gitHub: "vikrant0017",
+  instagram: "",
+  linkedIn: "vikrant-pradhan-06726a281",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
